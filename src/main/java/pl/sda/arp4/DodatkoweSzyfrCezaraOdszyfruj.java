81567a8d5221765c14d7f95c1d37935eeb4f46d3
Paweł Recław <preclaw@purestorage.com>
2022-06-05 21:37:19 +0200

    Szyfr cezara

---
+++ src/main/java/pl/sda/arp4/DodatkoweSzyfrCezaraOdszyfruj.java
@@ -0,0 +1,44 @@
+package pl.sda.arp4;
+
+import java.util.Scanner;
+
+public class DodatkoweSzyfrCezaraOdszyfruj {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj tekst:");
+        String tekst = scanner.nextLine();
+
+        System.out.println("Podaj liczbę która będzie kluczem szyfru:");
+        int klucz = scanner.nextInt();
+
+        // robimy kopię
+        char[] wynik = tekst.toCharArray();
+        for (int i = 0; i < wynik.length; i++) {
+            int litera = wynik[i];
+
+            if (Character.isAlphabetic(litera)) {
+                if (Character.isUpperCase(litera)) {
+                    litera -= 'A';
+                    litera -= klucz;
+                    while (litera < 0) {
+                        litera += 25;
+                    }
+                    litera = litera % 25;
+                    litera += 'A';
+                } else if (Character.isLowerCase(litera)) {
+                    litera -= 'a';
+                    litera -= klucz;
+                    while (litera < 0) {
+                        litera += 25;
+                    }
+                    litera = litera % 25;
+                    litera += 'a';
+                }
+            }
+            wynik[i] = (char) litera;
+        }
+
+        String wynikString = new String(wynik);
+        System.out.println("Wynik: " + wynikString);
+    }
+}
